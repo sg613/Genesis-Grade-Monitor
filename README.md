0f25e7d4160bfaba7e2090cd82bb1581e9941fa2
Steven Gatanas <steviegatanas@gmail.com>
2022-02-12 02:17:22 -0500

    Fresh Commit - No History

---
+++ README.md
@@ -0,0 +1,24 @@
+# Genesis-Grade-Monitor
+Program that uses Python to monitor grade updates in the Genesis Platform
+
+Guide:
+    Install by either cloning the repo or downloading the zip
+    
+   **ONCE INSTALLED YOU MUST RUN THE COMMAND ``pip install -r requirements.txt``**
+   **NECESARY STEPS TO RUN THE PROGRAM BELOW**
+    
+    Open the login.txt file and replace each placeholder with your information
+    
+    **YOU MUST REPLACE EACH ITEM IN THE SAME LINE THE PLACEHOLDER WAS**
+    
+    For the first line, replace with your email that you use for your genesis account
+    
+    For the second line, replace with your password for your genesis account
+    
+    For the third line, replace with your username (may be student id, = **NOT YOUR FULL EMAIL**)
+    
+    For the fourth line, replace with your school's genesis id (look at the link from your genesis home page and copy what is between the two slashes https://students.genesisedu.com/(Your schools genesis id should be here)/
+   
+    For the fifth line, replace with your discord webhook (heres a video on how to get one **Completely Free!!** https://www.youtube.com/watch?v=fKksxz2Gdnc
+    
+THANK YOU SO MUCH HOPE THIS WORKS!!! If you have any issues/errors, please let me know through this github repo (https://github.com/sg613/Genesis-Grade-Monitor/issues)
---
+++ main.py
@@ -0,0 +1,84 @@
+# Python Genesis Grade Monitor
+
+import requests
+from bs4 import BeautifulSoup
+import time
+from discord import Webhook, RequestsWebhookAdapter
+
+i = 0
+
+grades = []
+classes = []
+
+with open(r"login.txt", 'r') as f:
+    schoolID = f.readlines()[3]
+    sklID = schoolID.split('\n')
+    sID = sklID[0]
+with open(r"login.txt", 'r') as f:
+    webhookUrl = f.readlines()[4]
+
+
+loginUrl = f"https://students.genesisedu.com/{sID}/sis/j_security_check"
+
+
+webhook = Webhook.from_url(f"{webhookUrl}", adapter=RequestsWebhookAdapter())
+
+
+
+with open(r"login.txt", 'r') as f:
+    email = f.readlines()[0]
+
+with open(r"login.txt", 'r') as f:
+    password = f.readlines()[1]
+
+with open(r"login.txt", 'r') as f:
+    aID = f.readlines()[2]
+    bID = aID.split('\n')
+    id = bID[0]
+
+payload = {'j_username': email, 'j_password': password}
+
+
+with requests.session() as s:
+    a = s.post(loginUrl, data = payload)
+    result = s.get(f'https://students.genesisedu.com/{sID}/parents?tab1=studentdata&tab2=gradebook&tab3=weeklysummary&action=form&studentid={id}')
+    soup = BeautifulSoup(result.text, "html.parser")
+    while i < 99:
+        try:
+            graderaw = soup.find_all("div", {"style": "text-decoration: underline"})[i]
+            grade = graderaw.text.strip()
+            grades.append(grade)
+            className = soup.find_all("span", {"class": "categorytab"})[i]
+            classer = className.text.strip()
+            classes.append(classer)
+            i += 1
+        except:
+            i += 99
+
+
+x = 1
+n = 0
+newgrades = []
+
+while x == 1:
+    print("Monitoring...")
+    with requests.session() as h:
+        h.post(loginUrl, data = payload)
+        result2 = h.get(f'https://students.genesisedu.com/{sID}/parents?tab1=studentdata&tab2=gradebook&tab3=weeklysummary&action=form&studentid={id}')
+        zoup = BeautifulSoup(result2.text, "html.parser")
+        
+        for allC in classes:
+            
+            newgraderaw = zoup.find_all("div", {"style": "text-decoration: underline"})[n]
+            newgrade = newgraderaw.text.strip()
+            newgrades.append(newgrade)
+            n += 1
+
+    p = 0
+    for allClasses in classes:
+        if newgrades[p] != grades[p]:
+            webhook.send(f"Your {classes[p]} grade changed from a {grades[p]} to a {newgrades[p]}.")
+            grades[p] = newgrades[p]
+        p += 1
+    
+    time.sleep(300)
